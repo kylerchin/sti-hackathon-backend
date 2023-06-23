d59f3b2d41c0792248b5697e6857755249775dd1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-23 01:53:27 -0700

    add index

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn index(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+        .insert_header(("Server", "Actix"))
+        .insert_header(("Content-Type", "text/plain"))
+        .insert_header(("Access-Control-Allow-Origin", "*"))
+        .body("Hello world!")
+}
+
+#[actix_web::main]
+fn main() -> std::io::Result<()> {
+    // Create a new HTTP server.
+    let builder = HttpServer::new(|| App::new().route("/", web::get().to(index))).workers(4);
+
+    // Bind the server to port 8080.
+    let _ = builder.bind("127.0.0.1:8080").unwrap().run().await;
+
+    Ok(())
 }
